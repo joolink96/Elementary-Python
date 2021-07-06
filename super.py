ba933363f80df12271b27151d44a8b1e5569a89f
joolink96 <86863550+joolink96@users.noreply.github.com>
2021-07-06 20:28:53 +0900

    Add files via upload

---
+++ super.py
@@ -0,0 +1,24 @@
+#super
+
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+
+
+class Flyable:
+     def __init__(self):
+         print("Flyable 생성자")
+
+
+class FlyableUnit(Flyable,Unit):
+    def __init__(self):
+        #super().__init__() #self 는 없음
+        Unit.__init__(self)
+        Flyable.__init__(self)
+
+
+
+# 드랍쉽
+
+dropship= FlyableUnit()
